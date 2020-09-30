39722e3ebd321af9f496d2d83d91eb8fa66c0207
fariv indra <farivind@gmail.com>
2020-09-30 20:05:15 +0700

    done

--- 6.js
+++ 6.js
@@ -30,32 +30,3 @@ sentencesManipulation("ibu pergi ke pasar bersama aku");
 // ibu ergipnyo eknyo asarpnyo ersambnyo aku
 
 
-
-// }
-// for (let anakBaru of arrayBaru) {
-//     console.log(arrayBaru)
-// }
-//ibu
-//ergipnyo (else)
-//eknyo (else)
-//asarpnyo (else)
-//ersamabnyo (else)
-//aku
-
-//jidiin array dulu
-/*
- flet array baru = sentence.split(' ')
-*/
-
-
-
-    // for (let i = 0; i < arrayBaru; i++ ){
-    // if (arrayBaru[i][0] == "a" || arrayBaru[0] == "i" || arrayBaru[0] == "u" || arrayBaru[0] == "e" || arrayBaru[0] == "o"){
-    //     // console.log(arrayBaru[i]);
-    // } 
-//     else {  
-//       let a = arrayBaru.slice(0, 1)
-//       let b = arrayBaru.slice(1)
-
-//         console.log(b.concat(a, "nyo"))
-//   }
