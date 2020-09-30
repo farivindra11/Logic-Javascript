f4a1f4e9b3196742b34e4b01f9fc828bb3a3722f
fariv indra <farivind@gmail.com>
2020-09-30 20:04:55 +0700

    done

--- 3.js
+++ 3.js
@@ -1,3 +1,30 @@
+function romawi(n){
+ 
+    let desimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];                              
+    let konversi = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];            
+  
+    let hasil = ""; 
+  
+    for(let i = 0; i < desimal.length; i++){
+      while (desimal[i] <= n) {  
+        hasil += konversi[i];
+        n -= desimal[i];
+      }
+    }
+    return hasil;
+  }
+  
+console.log("Script Testing untuk Konversi Romawi\n");
+console.log("input | expected | result");
+console.log("------|----------|-------");
+console.log("4     | IV       | ", romawi(4));
+console.log("9     | IX       | ", romawi(9));
+console.log("13    | XIII     | ", romawi(13));
+console.log("1453  | MCDLIII  | ", romawi(1453));
+console.log("1646  | MDCXLVI  | ", romawi(1646));
+
+  
+
 // M=1000
 // D=500
 // C=100
@@ -49,30 +76,3 @@ console.log("1646  | MDCXLVI  | ", romawi(1646));
 // X=10
 // V=5
 // I=1
-
-function romawi(n){
- 
-    let desimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];                              
-    let konversi = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];            
-  
-    let hasil = ""; 
-  
-    for(let i = 0; i < desimal.length; i++){
-      while (desimal[i] <= n) {  
-        hasil += konversi[i];
-        n -= desimal[i];
-      }
-    }
-    return hasil;
-  }
-  
-console.log("Script Testing untuk Konversi Romawi\n");
-console.log("input | expected | result");
-console.log("------|----------|-------");
-console.log("4     | IV       | ", romawi(4));
-console.log("9     | IX       | ", romawi(9));
-console.log("13    | XIII     | ", romawi(13));
-console.log("1453  | MCDLIII  | ", romawi(1453));
-console.log("1646  | MDCXLVI  | ", romawi(1646));
-
-  
